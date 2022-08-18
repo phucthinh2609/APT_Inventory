fc0d194605252709c4fb1b036f99e9b50616eca3
unknown <anhlee.2013@gmail.com>
2022-08-18 15:54:33 +0700

    create table

---
+++ apt-inventory/src/main/java/com/cg/model/LocationDelivery.java
@@ -0,0 +1,49 @@
+package com.cg.model;
+
+
+import lombok.*;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "location_deliveries")
+@Accessors(chain = true)
+public class LocationDelivery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "province_id")
+    private String provinceId;
+
+    @Column(name = "province_name")
+    private String provinceName;
+
+    @Column(name = "district_id")
+    private String districtId;
+
+    @Column(name = "district_name")
+    private String districtName;
+
+    @Column(name = "ward_id")
+    private String wardId;
+
+    @Column(name = "ward_name")
+    private String wardName;
+
+    private String address;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+
+
+}
--- apt-inventory/src/main/java/com/cg/model/Product.java
+++ apt-inventory/src/main/java/com/cg/model/Product.java
@@ -1,12 +1,14 @@
 package com.cg.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.Accessors;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import javax.validation.constraints.Digits;
+import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -17,12 +19,25 @@ import javax.persistence.*;
 @Table(name = "products")
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
 
     private String title;
 
+    @Column(name = "product_code")
+    private String productCode;
 
+    @Digits(integer = 10, fraction = 0)
+    @Column(name = "stock_in_price")
+    private BigDecimal stockInPrice;
+
+    @Digits(integer = 10, fraction = 0)
+    @Column(name = "purchase_order_price")
+    private BigDecimal purchaseOrderPrice;
 
     private String description;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private Set<Inventory> inventories;
 }
