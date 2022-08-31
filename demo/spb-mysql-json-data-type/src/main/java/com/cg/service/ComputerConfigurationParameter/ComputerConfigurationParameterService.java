ade6a21c54c3b6d056c8f6a61b247e8b41b561ed
unknown <anhlee.2013@gmail.com>
2022-08-31 10:05:07 +0700

    delete demo

--- demo/spb-mysql-json-data-type/src/main/java/com/cg/service/ComputerConfigurationParameter/ComputerConfigurationParameterService.java
+++
@@ -1,7 +0,0 @@
-package com.cg.service.ComputerConfigurationParameter;
-
-import com.cg.model.ComputerConfigurationParameter;
-import com.cg.service.IGeneralService;
-
-public interface ComputerConfigurationParameterService extends IGeneralService<ComputerConfigurationParameter> {
-}
