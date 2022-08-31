d00f34006f2314bf203e811b989e1d4c4480227a
unknown <anhlee.2013@gmail.com>
2022-08-31 16:06:55 +0700

    sql

---
+++ demo/spb-mysql-json-data-type/src/main/java/com/cg/service/ComputerConfigurationParameter/ComputerConfigurationParameterService.java
@@ -0,0 +1,7 @@
+package com.cg.service.ComputerConfigurationParameter;
+
+import com.cg.model.ComputerConfigurationParameter;
+import com.cg.service.IGeneralService;
+
+public interface ComputerConfigurationParameterService extends IGeneralService<ComputerConfigurationParameter> {
+}
